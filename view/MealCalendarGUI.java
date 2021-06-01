d8e969fc84055efb84eec5a0049f16b6b0c8d5fc
EzraDryerBU <edryer1@binghamton.edu>
2021-06-01 00:59:23 -0500

    gui now has buttons and some text

--- view/MealCalendarGUI.java
+++ view/MealCalendarGUI.java
@@ -1,7 +1,9 @@
 package mealCalendar.view;
 
+import java.awt.Dimension;
+import java.awt.Font;
+
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -19,14 +21,11 @@ public class MealCalendarGUI {
 	private JTabbedPane calendarPane;
 	private JScrollPane scroller;
 	private JPanel panel;
-	//private JComboBox<Integer> jComboDay;
-	//private JComboBox<Integer> jComboMonth;
-	//private JComboBox<Integer> jComboYear;
 	private SpringLayout layout;
 	
 	
 	public void setModel(MealCalendar m) {
-		mealCalendar = m;		
+		mealCalendar = m;	
 	}
 	
 	private void createAndShowGUI() {
@@ -36,33 +35,56 @@ public class MealCalendarGUI {
 		frame.addWindowListener(WindowListenerFactory.windowClosingFactory(e -> exit()));
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-		
+		Font font = new Font("Serif", Font.PLAIN, 14);
 		panel = new JPanel();
 		layout = new SpringLayout();
 		panel.setLayout(layout);
 		panel.setSize(300,300);
 		
 		JLabel nextMealsLabel = new JLabel("Please input the amount of meals to retrieve here: ");
+		nextMealsLabel.setFont(font);
 		panel.add(nextMealsLabel);
 		layout.putConstraint(SpringLayout.NORTH, nextMealsLabel, 200, SpringLayout.WEST, panel);
-		layout.putConstraint(SpringLayout.WEST, nextMealsLabel, 50, SpringLayout.WEST, panel);
+		layout.putConstraint(SpringLayout.WEST, nextMealsLabel, 10, SpringLayout.WEST, panel);
 		
 		JButton nextXMealsButton = new JButton("Get Next X Amount of Meals");
-		//nextXMealsButton
+		nextXMealsButton.setPreferredSize(new Dimension(215,30));
+		nextXMealsButton.setFont(font);
+		//need to add a listener event for when this button is clicked
 		panel.add(nextXMealsButton);
+		layout.putConstraint(SpringLayout.NORTH, nextXMealsButton, 192, SpringLayout.WEST, panel);
+		layout.putConstraint(SpringLayout.WEST, nextXMealsButton, 350, SpringLayout.WEST, panel);
+		
+		JTextField nextXMealsInput = new JTextField();
+		nextXMealsInput.setEditable(true);
+		nextXMealsInput.setPreferredSize(new Dimension(45, 20));
+		panel.add(nextXMealsInput);
+		layout.putConstraint(SpringLayout.NORTH, nextXMealsInput, 200, SpringLayout.WEST, panel);
+		layout.putConstraint(SpringLayout.WEST, nextXMealsInput, 300, SpringLayout.WEST, panel);
+		
+		JButton nextMonthsMeals = new JButton("Get a Months Worth of Meals");
+		nextMonthsMeals.setPreferredSize(new Dimension(215, 30));
+		nextMonthsMeals.setFont(font);
+		//need an action listener
+		panel.add(nextMonthsMeals);
+		layout.putConstraint(SpringLayout.NORTH, nextMonthsMeals, 100, SpringLayout.WEST, panel);
+		layout.putConstraint(SpringLayout.WEST, nextMonthsMeals, 350, SpringLayout.WEST, panel);
 		
 		
-		
-		JButton nextMonthsMeals;
-		JButton getRandomMeal;
-		JTextField nextXMealsInput;
+		JButton getRandomMeal = new JButton("Get a Random Meal");		
+		getRandomMeal.setPreferredSize(new Dimension(215, 30));
+		getRandomMeal.setFont(font);
+		//need an action listener
+		panel.add(getRandomMeal);
+		layout.putConstraint(SpringLayout.NORTH, getRandomMeal, 20, SpringLayout.WEST, panel);
+		layout.putConstraint(SpringLayout.WEST, getRandomMeal, 350, SpringLayout.WEST, panel);
 		
 		
 		calendarPane = new JTabbedPane();
 		scroller = new JScrollPane(panel);
 		calendarPane.add("Input", scroller);
 		frame.add(calendarPane);
-		
+		//frame.pack(); //messing with this, might be useful
 	}
 	
 	private void exit() {
